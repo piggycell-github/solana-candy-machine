0bbfc266819bca34fb4302db119a76adaca93c50
bsangs <tkdals6805@naver.com>
2024-12-18 14:19:18 +0900

    rent withdraw

---
+++ rentWithdraw.ts
@@ -0,0 +1,21 @@
+import { deleteCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import dotenv from "dotenv";
+import { candyMachineSigner, umi } from "./base";
+
+dotenv.config();
+
+async function withdrawRent() {
+  try {
+    // 렌트 출금 트랜잭션 실행
+    const withdrawTx = await deleteCandyMachine(umi, {
+      candyMachine: candyMachineSigner.publicKey,
+    }).sendAndConfirm(umi);
+
+    console.log("렌트 출금이 완료되었습니다.");
+    console.log("트랜잭션 서명:", withdrawTx.signature);
+  } catch (error) {
+    console.error("렌트 출금 중 오류가 발생했습니다:", error);
+  }
+}
+
+withdrawRent();
